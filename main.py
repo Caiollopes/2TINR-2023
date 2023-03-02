94299edd110e76b8feea2cc06cc7b1efa2b2e256
Caiollopes <101848228+Caiollopes@users.noreply.github.com>
2023-03-02 19:52:19 -0300

    create func exibir & remove ta
    create func exibir & remove ta

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("ta")
+dicionario = {12345:["Caio", "Aluno"]}
+
+def exibir():
+  for chave,lista in dicionario.items():
+    print("Cod. do funcionario...", chave)
+    print("Nome...", lista[0])
+    print("Cargo...", lista[1])
